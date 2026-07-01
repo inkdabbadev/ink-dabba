2489be8e9df0d3aaf750db439ff277cef73397b2
inkdabbadev <inkdabba.dev@gmail.com>
2026-07-01 16:40:29 +0530

    added admin on aanandhass

---
+++ app/anandhaas/admin/page.js
@@ -0,0 +1,29 @@
+
+export const metadata = {
+  title: "Your Masked Title",
+};
+
+export default function Page() {
+  return (
+    <main
+      style={{
+        margin: 0,
+        padding: 0,
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <iframe
+        src="https://anandhaas-fifa.vercel.app/admin"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+          display: "block",
+        }}
+        title="Embedded Website"
+      />
+    </main>
+  );
+}
