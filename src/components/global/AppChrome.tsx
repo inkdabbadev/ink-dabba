835d1d1332bfbee03db0306dbd3cfca77b897d44
developers-inkdabba <developers@inkdabba.com>
2026-09-14 15:13:56 +0530

    /PUZZLE changed and floating removed

--- src/components/global/AppChrome.tsx
+++ src/components/global/AppChrome.tsx
@@ -4,7 +4,6 @@ import { usePathname } from "next/navigation";
 import type { ReactNode } from "react";
 import { CursorProvider } from "@/context/CursorContext";
 import CustomCursor from "@/components/global/CustomCursor";
-import FloatingBadge from "@/components/global/FloatingBadge";
 import Navigation from "@/components/global/Navigation";
 import SmoothScroll from "@/components/global/SmoothScroll";
 
@@ -29,7 +28,6 @@ export default function AppChrome({ children }: { children: ReactNode }) {
       <CustomCursor />
       <Navigation />
       <SmoothScroll>{children}</SmoothScroll>
-      <FloatingBadge />
     </CursorProvider>
   );
 }
